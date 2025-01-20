bfd41a76962880a5e6f2f07581529572143a9d40
Scott <tsgcats@gmail.com>
2025-01-20 05:59:54 -0500

    started Number Guessing Game

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 "use strict";
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Function to check the user's guess
+function checkGuess(secretNumber, userGuess) {
+    if (userGuess < secretNumber) {
+        return "Too low! Try again.";
+    } else if (userGuess > secretNumber) {
+        return "Too high! Try again.";
+    } else {
+        return "Congratulations! You guessed the correct number!";
+    }
+}
